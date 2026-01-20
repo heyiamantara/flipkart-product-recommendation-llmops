95d7f6b7f1969424d6309a6a97b8c837868a2988
heyiamantara <antara00418@gmail.com>
2026-01-20 12:37:38 +0000

    Fix RAG chain imports for modern LangChain

--- flipkart/rag_chain.py
+++ flipkart/rag_chain.py
@@ -1,24 +1,19 @@
-from langchain_groq import ChatGroq
 
+from langchain_groq import ChatGroq
 from langchain.chains.history_aware_retriever import create_history_aware_retriever
 from langchain.chains.retrieval import create_retrieval_chain
-from langchain.chains.combine_documents.stuff import create_stuff_documents_chain
-
+from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain_community.chat_message_histories import ChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
-
 from flipkart.config import Config
 
 
 class RAGChainBuilder:
     def __init__(self, vector_store):
         self.vector_store = vector_store
-        self.model = ChatGroq(
-            model=Config.RAG_MODEL,
-            temperature=0.5
-        )
+        self.model = ChatGroq(model=Config.RAG_MODEL, temperature=0.5)
         self.history_store = {}
 
     def _get_history(self, session_id: str) -> BaseChatMessageHistory:
@@ -30,31 +25,25 @@ class RAGChainBuilder:
         retriever = self.vector_store.as_retriever()
 
         contextualize_prompt = ChatPromptTemplate.from_messages([
-            ("system", "You are a helpful assistant."),
+            ("system", "Rephrase the question using chat history."),
             MessagesPlaceholder("chat_history"),
             ("human", "{input}")
         ])
 
         history_aware_retriever = create_history_aware_retriever(
-            self.model,
-            retriever,
-            contextualize_prompt
+            self.model, retriever, contextualize_prompt
         )
 
         qa_prompt = ChatPromptTemplate.from_messages([
-            ("system", "Answer using the provided context."),
+            ("system", "Answer using provided context."),
             MessagesPlaceholder("chat_history"),
             ("human", "{input}")
         ])
 
-        doc_chain = create_stuff_documents_chain(
-            self.model,
-            qa_prompt
-        )
+        qa_chain = create_stuff_documents_chain(self.model, qa_prompt)
 
         rag_chain = create_retrieval_chain(
-            history_aware_retriever,
-            doc_chain
+            history_aware_retriever, qa_chain
         )
 
         return RunnableWithMessageHistory(
@@ -63,3 +52,4 @@ class RAGChainBuilder:
             input_messages_key="input",
             history_messages_key="chat_history"
         )
+
